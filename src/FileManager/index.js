bd7336b00f9ae643a65b78bc639a9fe080291867
Drew Jenkel <drew@hitchsoftware.com>
2025-04-09 09:39:39 -0400

    Patching for Dark Mode

--- src/FileManager/index.js
+++ src/FileManager/index.js
@@ -1,1 +1,4 @@
+// REPO: @hitchsoftware/react-file-manager
+// FILE: src\FileManager\index.js
+
 export { default } from "./FileManager";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// REPO: @hitchsoftware/react-file-manager
+// FILE: src\index.js
+
+import { applyAutoTheme } from "./styles/initTheme.js";
+
+applyAutoTheme();
+
 export { default as FileManager } from "./FileManager/FileManager";
