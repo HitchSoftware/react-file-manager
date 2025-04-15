aa586df030cd37608058d6ee498d79b62f853556
Drew Jenkel <drew@hitchsoftware.com>
2025-04-15 17:21:29 -0400

    useFileNavigation export

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ import { applyAutoTheme } from "./styles/initTheme";
 applyAutoTheme();
 
 export { FileManager } from "./FileManager/FileManager";
+export { useFileNavigation } from "./contexts/FileNavigationContext";
 export type { FileManagerProps, FileUploadConfig } from "./FileManager/FileManager";
 export type { FileEntity } from "./types/FileEntity";
+
